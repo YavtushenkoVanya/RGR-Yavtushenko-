f2b3e604409530beb4b9d0f49b6b85e95d47893f
YavtushenkoVanya <128784289+YavtushenkoVanya@users.noreply.github.com>
2023-06-04 10:54:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,130 @@
+let mySwiper = new Swiper('.swiper', {
+    spaceBetween: 80,
+    slidesPerView: 3,
+    centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+    loopAdditionalSlides: 25,
+    navigation: {
+        nextEl: ".swiper-button-n",
+        prevEl: ".swiper-button-p"
+    }
+});
+
+
+const openPopUp = document.getElementById("open_popup1");
+const colosePopUp = document.getElementById("close_popup1");
+const popUp = document.getElementById("popup1");
+
+openPopUp.addEventListener("click", function(e){
+    e.preventDefault();
+    popUp.classList.add("active");
+})
+
+colosePopUp.addEventListener("click", () =>{
+    popUp.classList.remove("active");
+})
+
+const openPopUp2 = document.getElementById("open_popup2");
+const colosePopUp2 = document.getElementById("close_popup2");
+const popUp2 = document.getElementById("popup2");
+
+openPopUp2.addEventListener("click", function(e2){
+    e2.preventDefault();
+    popUp2.classList.add("active");
+})
+
+colosePopUp2.addEventListener("click", () =>{
+    popUp2.classList.remove("active");
+})
+
+const openPopUp3 = document.getElementById("open_popup3");
+const colosePopUp3 = document.getElementById("close_popup3");
+const popUp3 = document.getElementById("popup3");
+
+openPopUp3.addEventListener("click", function(e3){
+    e3.preventDefault();
+    popUp3.classList.add("active");
+})
+
+colosePopUp3.addEventListener("click", () =>{
+    popUp3.classList.remove("active");
+})
+
+const openPopUp4 = document.getElementById("open_popup4");
+const colosePopUp4 = document.getElementById("close_popup4");
+const popUp4 = document.getElementById("popup4");
+
+openPopUp4.addEventListener("click", function(e4){
+    e4.preventDefault();
+    popUp4.classList.add("active");
+})
+
+colosePopUp4.addEventListener("click", () =>{
+    popUp4.classList.remove("active");
+})
+
+const openPopUp5 = document.getElementById("open_popup5");
+const colosePopUp5 = document.getElementById("close_popup5");
+const popUp5 = document.getElementById("popup5");
+
+openPopUp5.addEventListener("click", function(e5){
+    e5.preventDefault();
+    popUp5.classList.add("active");
+})
+
+colosePopUp5.addEventListener("click", () =>{
+    popUp5.classList.remove("active");
+})
+
+const openPopUp6 = document.getElementById("open_popup6");
+const colosePopUp6 = document.getElementById("close_popup6");
+const popUp6 = document.getElementById("popup6");
+
+openPopUp6.addEventListener("click", function(e6){
+    e6.preventDefault();
+    popUp6.classList.add("active");
+})
+
+colosePopUp6.addEventListener("click", () =>{
+    popUp6.classList.remove("active");
+})
+
+const openPopUp7 = document.getElementById("open_popup7");
+const colosePopUp7 = document.getElementById("close_popup7");
+const popUp7 = document.getElementById("popup7");
+
+openPopUp7.addEventListener("click", function(e7){
+    e7.preventDefault();
+    popUp7.classList.add("active");
+})
+
+colosePopUp7.addEventListener("click", () =>{
+    popUp7.classList.remove("active");
+})
+
+const openPopUp8 = document.getElementById("open_popup8");
+const colosePopUp8 = document.getElementById("close_popup8");
+const popUp8 = document.getElementById("popup8");
+
+openPopUp8.addEventListener("click", function(e8){
+    e8.preventDefault();
+    popUp8.classList.add("active");
+})
+
+colosePopUp8.addEventListener("click", () =>{
+    popUp8.classList.remove("active");
+})
+
+const openPopUp9 = document.getElementById("open_popup9");
+const colosePopUp9 = document.getElementById("close_popup9");
+const popUp9 = document.getElementById("popup9");
+
+openPopUp9.addEventListener("click", function(e9){
+    e9.preventDefault();
+    popUp9.classList.add("active");
+})
+
+colosePopUp9.addEventListener("click", () =>{
+    popUp9.classList.remove("active");
+})
